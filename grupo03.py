4fea11605b42a755202c710cdc8b08478d863e9d
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2020-06-12 20:38:14 -0300

    Update grupo03.py
    Complete el primer prototipo del metodo ObtenerFollow. Le fataria poner algunos comentarios para explicar mejor su funcionamiento.

--- grupo03.py
+++ grupo03.py
@@ -68,12 +68,11 @@ class Gramatica():
     def ObtenerFirst(Regla):
   
       First = [] 
-      """la regla es un diccionario, su clave (antecedente) es un string y el consecuente debe ser un array o lista que contanga los elementos de la misma separados (por causa del espacio)"""
+      """la Regla es un diccionario, su clave (antecedente) es un string y el consecuente debe ser un array o lista que contanga los elementos de la misma separados (por causa de los espacios)"""
       antecedente = list(Regla.keys())[0]
       consecuente = Regla[antecedente]
- 
-      PrimerElementoDelConsecuente = consecuente[0]
- 
+
+      PrimerElementoDelConsecuente = consecuente[0] 
 
       if "lambda" in consecuente:
         First.append("lambda")
@@ -82,30 +81,54 @@ class Gramatica():
         First.append(PrimerElementoDelConsecuente)
       else:    
         """ahora sabemos que PrimerElementoDelConsecuente es un no terminal"""
-        for NuevoConsecuente in ReglasGramaticales[PrimerElementoDelConsecuente]:
-          """recorro las reglas buscando cada consecuente que tenga a """
-          NuevaRegla = { PrimerElementoDelConsecuente : NuevoConsecuente.split(" ") }
+        for consecuente_1 in self.ReglasGramaticales[PrimerElementoDelConsecuente]:
+          """recorro las reglas buscando cada consecuente que tenga el no terminal como antecedente, y sacamos su first"""
+          NuevaRegla = { PrimerElementoDelConsecuente : consecuente_1.split(" ") }
           First.extend(ObtenerFirst(NuevaRegla))
     
         if "lambda" in First:         
           First.remove("lambda")
           consecuente.remove(PrimerElementoDelConsecuente)
           NuevaRegla = {antecedente : consecuente}
-          if NuevoConsecuente:
+          if consecuente:
             First.extend(ObtenerFirst(NuevaRegla))
           else:
             First.append("lambda") 
 
       return list(OrderedDict.fromkeys(First))
-    def ObtenerFollow(Antecedente):
-  
-      Follow = []
-  
-      if Antecedente == Axioma:
-        Follow.append("$")
+   
+    def ObtenerFollow(antecedente_1):
+        """ Este if es para utilizar mas abajo y fasilitar el convocar el metodo de follow dentro de si mismo """
+        if antecedente_1.islower():
+            return [antecedente_1]
 
-      for antecedente, consecuentes in ReglasGramaticales:
-        if Antecedente in consecuentes:
-            x = 1
-      return  list(OrderedDict.fromkeys(Follow))
+        Follow = []
+  
+        if antecedente_1 == Axioma:
+            Follow.append("$")
+
+        for antecedente_2, consecuentes in self.ReglasGramaticales.items():
+            for consecuente_1 in consecuentes:
+                
+                consecuente_2 = consecuente_1.split(" ")
+                """ recorremos cada consecuente de cada antecedente de las reglas buscando ul antecedente a sacar su follow """
+                if antecedente_1 in consecuente_2 :
+                    ubicacion = consecuente_2.index(antecedente_1)
+                    tamaño_consecuente_2 = len(consecuente_2)
+                    while (ubicacion + 1) < tamaño_consecuente_2:
+                        SiguienteElemento = consecuente_2[ubicacion + 1]
+                        if consecuente_2[ubicacion + 1].islower():
+                            Follow.append(SiguienteElemento)
+                            break
+                        elif antecedente_1 != antecedente_2:
+                            Follow.extend(ObtenerFirst({antecedente_2:list(SiguienteElemento)}))
+                            if "lambda" in Follow:
+                                Follow.remove("lambda")
+                                Follow.extend(ObtenerFollow(SiguienteElemento))
+                                ubicacion = ubicacion + 1
+                    if ((ubicacion + 1) == tamaño_consecuente_2) and (antecedente_1 != antecedente_2):
+                        Follow.extend(ObtenerFollow(antecedente_2))
+
+  
+        return  list(OrderedDict.fromkeys(Follow))
 
